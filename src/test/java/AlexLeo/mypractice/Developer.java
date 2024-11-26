a44fb72c76061e1d902f417a1e6cf55e2cc6e947
mihaibordei <workbordeimihai@gmail.com>
2024-11-25 20:08:24 -0800

    modified POM

--- src/test/java/AlexLeo/mypractice/Developer.java
+++
@@ -1,9 +0,0 @@
-package AlexLeo.mypractice;
-
-public class Developer extends Employee {
-
-
-    Developer(String firstName1, String lastName1, String title1, double salary1, double workingYears1, String country1) {
-        super(firstName1, lastName1, title1, salary1, workingYears1, country1);
-    }
-}
